667b90e66ef5bb43f3db0e8fbc01222c64ac322d
Praxal <praxalshah@gmail.com>
2025-01-09 18:25:33 +0530

    Update Prod Deployment

--- prod_deploy.sh
+++ prod_deploy.sh
@@ -13,27 +13,11 @@ git pull
 msg "Building Docker image"
 sudo docker build --tag app .
 
-msg "Stopping Docker container"
-sudo docker stop app
-sudo docker rm app
-msg "Starting Docker container"
+msg "Stopping containers"
+sudo docker compose down
 
-sudo docker run \
--d \
---name app \
---expose 443 \
--p 443:443 \
--v /etc/letsencrypt:/etc/letsencrypt \
--e SERVER_ENV=PROD \
-app
-
-msg "Starting Postgres container"
-sudo docker run -d \
---name db-postgres \
--p 5432:5432 \
---mount type=volume,src=app-db,target=/var/lib/postgresql/data \
--e POSTGRES_PASSWORD=mysecretpassword \
-postgres:15.1-alpine
+msg "Starting containers"
+sudo docker compose up -d
 
 msg "Pruning stale Docker images"
 sudo docker image prune -f
